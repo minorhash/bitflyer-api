e1a77186df84dea17f9c143410171932105bd3fc
minorhash <minorhash@gmail.com>
2019-02-24 04:44:46 +0900

    02/24/19

--- fil/bal.js
+++
@@ -1,6 +0,0 @@
-var fs=require('fs');
-var request = require('request');
-var crypto = require('crypto');
-var config = JSON.parse(fs.readFileSync(__dirname+'/config.json', 'utf8'));
-
-console.log(config);
