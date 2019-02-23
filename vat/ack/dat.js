4c3ded1ccdb04a1a2f7d99a76c57bec6356ac6c7
minorhash <minorhash@gmail.com>
2019-02-24 04:47:23 +0900

    02/24/19

--- vat/ack/dat.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-var crypto = require('crypto');
-
-var key = 'PYBqczy4PbGyAnXVYj4knx';
-var secret = 'Q7fuC7zpKZoxXkK+YAx1DBOvt9Rde7U06XZcWRglYlo=';
-
-var dat= Date.now().toString();
-
-console.log(dat);
